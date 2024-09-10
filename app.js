a4084e7d1be9cc6236aa096eb48e695a24ecefa3
shtrv <71714403+shtrv@users.noreply.github.com>
2024-09-10 13:10:22 +0300

    splitAndSortFile, sortAndSaveChunk

---
+++ app.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const maxChunkSize = 500*1024*1024;
+const inputFile = 'largefile.txt';
+const outputFile = 'sorted_largefile.txt'
+const outputDir = './result';
+const tmpDir = path.join('./', 'sorted_chunks');
+if (!fs.existsSync(tmpDir)) {
+    fs.mkdirSync(tmpDir);
+}
+
+async function splitAndSortFile(inputFile, maxChunkSize) {
+    const fileStream = fs.createReadStream(inputFile);
+    const rl = readline.createInterface({input: fileStream});
+
+    let chunk = [];
+    let chunkIndex = 0;
+    let chunkSize = 0;
+
+    for await (const line of rl) {
+        chunk.push(line);
+        chunkSize += Buffer.byteLength(line, 'utf8');
+
+        if (chunkSize >= maxChunkSize) {
+            await sortAndSaveChunk(chunk, chunkIndex);
+            chunk = [];
+            chunkSize = 0;
+            chunkIndex++;
+        }
+    }
+
+    if (chunk.length > 0) {
+        await sortAndSaveChunk(chunk, chunkIndex);
+    }
+
+    fileStream.close();
+}
+
+async function sortAndSaveChunk(chunk, chunkIndex) {
+    chunk.sort();
+    const sortedChunkFileName = path.join(tmpDir, `${chunkIndex}`);
+    fs.writeFileSync(sortedChunkFileName, chunk.join('\n') + '\n', 'utf-8')
+}
+
+splitAndSortFile(inputFile, maxChunkSize);
