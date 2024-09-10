ae83ef0f99c3f39f933af24327f1db4154a5972b
shtrv <71714403+shtrv@users.noreply.github.com>
2024-09-10 15:06:47 +0300

    added merge func

--- app.js
+++ app.js
@@ -45,6 +45,32 @@ async function sortAndSaveChunk(chunk, chunkIndex) {
     fs.writeFileSync(sortedChunkFileName, chunk.join('\n'), 'utf-8')
 }
 
+async function mergeChunks(outputFile) {
+    const chunkFiles = fs.readdirSync(tmpDir).map(file => path.join(tmpDir, file));
+    const outputStream = fs.createWriteStream(outputFile, { encoding: 'utf8' });
 
+    let chunkStreams = chunkFiles.map(file => readline.createInterface({ input: fs.createReadStream(file, { encoding: 'utf8' }) }));
+    let chunkLines = await Promise.all(chunkStreams.map(async stream => (await stream[Symbol.asyncIterator]().next()).value));
+    
+    while (chunkStreams.length > 0) {
+        let minIndex = 0;
+        for (let i = 1; i < chunkLines.length; i++) {
+            if (chunkLines[i] < chunkLines[minIndex]) {
+                minIndex = i;
+            }
+        }
 
-splitAndSortFile(inputFile, maxChunkSize);
+        outputStream.write(chunkLines[minIndex] + '\n');
+        const next = await chunkStreams[minIndex][Symbol.asyncIterator]().next();
+
+        if (next.done) {
+            chunkStreams.splice(minIndex, 1);
+            chunkLines.splice(minIndex, 1);
+        } else {
+            chunkLines[minIndex] = next.value;
+        }
+    }
+
+    outputStream.end();
+}
+
